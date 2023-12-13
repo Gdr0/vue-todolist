72429765c4abf188a6a8b2b4a339d19f7e4c9c77
Gabriele Di Rocco <gabriele.dirocco000@gmail.com>
2023-12-13 13:41:12 +0100

    aggiunta logica con vue

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+const { createApp } = Vue;
+
 createApp({
   data() {
     return {
+      message: "",
       todos: [
         {
           text: "Fare i compiti",
@@ -17,5 +20,17 @@ createApp({
       ],
     };
   },
-  methods: {},
+  methods: {
+    delTask(index) {
+      this.todos.splice(index, 1);
+    },
+    addTask() {
+      // if (this.message.trim() !== "") {
+      //   this.todos.push({ text: this.message });
+      //   this.message = "";
+      // }
+      this.todos.unshift({ text: this.message });
+      this.mesage = "";
+    },
+  },
 }).mount("#app");
